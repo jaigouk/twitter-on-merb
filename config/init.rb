ab47fa07c1f2686d40d06ef37b48fc53422dc71b
jaigouk <jaigouk@gmail.com>
2009-01-16 14:28:58 +0900

    still working on the codes

--- config/init.rb
+++ config/init.rb
@@ -20,17 +20,7 @@ dependency "dm-validations", dm_gems_version
 
 dependency "do_mysql", do_gems_version
 dependency "do_sqlite3", do_gems_version
-
-#dependency "daemons"
-dependency "daemon_controller"
-#dependency "merb_daemon"
-
-#dependency "dm-is-searchable", dm_gems_version 
-
-dependency "dm-core", dm_gems_version do
-#  DataMapper.setup(:default, "mysql::in_memory")
-#  DataMapper.setup(:search, "ferret://#{Pathname(__FILE__).dirname.expand_path.parent + "dbindex"}")
-end
+dependency "dm-core", dm_gems_version 
 
 dependency 'merb-cache', merb_gems_version do 
   Merb::Cache.setup do
@@ -43,35 +33,25 @@ dependency 'merb-cache', merb_gems_version do
    CACHE_SETUP = true
   end
 end
+dependency 'nokogiri'
+dependency 'shorturl'
+dependency 'god'
 
 gem('twitter4r', '0.3.0')
 require 'twitter'
 require 'time'
 require 'open-uri'
-dependency 'nokogiri'
-dependency 'shorturl'
 require 'memcache'
 
 Merb::BootLoader.before_app_loads do
-#  dev_gems = Dir.glob("#{Merb.root}/gems/development/**/lib/*.rb")
-#  dev_gems.each { |dev_gem| require dev_gem }
-
 end
 
 Merb::BootLoader.after_app_loads do
 
-  
-#  if  STARLING = MemCache.new('127.0.0.1:22122') then
   STARLING = MemCache.new('127.0.0.1:22122') 
-#      require Merb.root + '/config/daemon_controller.rb'
-#    puts 'starling loaded'
-#  end
-
 end
 
 
-    
-# Move this to application.rb if you want it to be reloadable in dev mode.
 Merb::Router.prepare do
   match('/').to(:controller => "twitter_on_merb", :action =>'index')
   match('/about').to(:controller => "twitter_on_merb", :action =>'about')
@@ -79,7 +59,7 @@ Merb::Router.prepare do
   default_routes
 end
 
-# I copied environments folder from normal app into config folder
+
 Merb::Config.use { |c|
   c[:environment]         = 'production',
   c[:framework]           = {},
